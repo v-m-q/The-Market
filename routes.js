df00a75965190ff23fa81d585835f14118df462a
aya maher <aya.maher.m7md@gmail.com>
2024-03-31 14:52:46 +0200

    Fix:delete routes and axios

--- routes.js
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import ProductDetails from "./src/Pages/ProductDetails/ProductDetails";
-
-const router = createBrowserRouter( [
-    {
-        path: '/products/:id',
-        Component:ProductDetails
-    }
-])
