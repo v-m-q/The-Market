de90e4a08dcb9c81f21ad83e45c6893e385bf09a
menna-abdallah <menna.abdallh11@gmail.com>
2024-04-13 06:27:39 +0200

    fix : order total items

--- src/Pages/orders/Orders.js
+++ src/Pages/orders/Orders.js
@@ -11,52 +11,48 @@ import "./Orders.css";
 
 export default function AllOrders() {
   const [orders, setOrders] = useState([]);
-  // const [initialFetchCompleted] = useState(false);
   const next = useSelector((state) => state.pages.next);
   const previous = useSelector((state) => state.pages.previous);
-  useEffect(() => {
-    getAllOrders()
-      .then((response) => {
-        const fetchedOrders = response.data.orders.map((order) => ({
-          ...order,
-          totalItems: 0,
-        }));
-          setOrders( fetchedOrders );
-          dispatch(changePage({next:fetchedOrders.data.next,previous:fetchedOrders.data.previous}))
-      })
-      .catch((error) => {
-        console.error("Error fetching orders:", error);
-      });
-  }, []);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchOrderItems = async () => {
-      const ordersWithItems = await Promise.all(
-        orders.map(async (order) => {
-          try {
-            const itemsResponse = await getAllOrdersItems(order.order_id);
-            const orderWithItems = {
-              ...order,
-              totalItems: itemsResponse.data.length,
-            };
-            return orderWithItems;
-          } catch (error) {
-            console.error(
-              `Error fetching items for order ${order.order_id}:`,
-              error
-            );
-            return order;
-          }
-        })
-      );
-      setOrders(ordersWithItems);
+    const fetchData = async () => {
+      try {
+        const ordersData = await getAllOrders();
+        const ordersWithTotalItems = await Promise.all(
+          ordersData.data.orders.map(async (order) => ({
+            ...order,
+            totalItems: await fetchTotalItems(order.order_id),
+          }))
+        );
+        setOrders(ordersWithTotalItems);
+        dispatch(
+          changePage({
+            next: ordersData.data.next,
+            previous: ordersData.data.previous,
+          })
+        );
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
     };
-    fetchOrderItems();
+    fetchData();
   }, []);
+
+  const fetchTotalItems = async (orderId) => {
+    try {
+      const response = await getAllOrdersItems(orderId);
+      return response.data.length;
+    } catch (error) {
+      console.error(`Error fetching items for order ${orderId}:`, error);
+      return 0;
+    }
+  };
+
   const goToNextPage = async () => {
     try {
       const nextOrdersData = await getOrdersForPage(next);
-      setProducts(nextOrdersData.data.results);
+      setOrders(nextOrdersData.data.results);
       dispatch(
         changePage({
           next: nextOrdersData.data.next,
@@ -67,10 +63,11 @@ export default function AllOrders() {
       console.log("there is no next products");
     }
   };
+
   const goToPreviousPage = async () => {
     try {
       const previousOrdersData = await getOrdersForPage(previous);
-      setProducts(previousOrdersData.data.results);
+      setOrders(previousOrdersData.data.results);
       dispatch(
         changePage({
           next: previousOrdersData.data.next,
@@ -123,29 +120,32 @@ export default function AllOrders() {
                   Created At: <span>{formatDate(order.created_at)}</span>
                 </li>
               </ul>
-              <Link className="button" to={`/order-details/${order.order_id}`}>
-                    Show Items
+              <Link
+                className="button"
+                to={`/order-details/${order.order_id}`}
+              >
+                Show Items
               </Link>
             </div>
           </div>
         ))}
-      <nav
-        aria-label="..."
-        className="col-lg-9 col-md-9 d-flex justify-content-center mt-3 w-100"
-      >
-        <ul class="pagination">
-          <li class="page-item">
-            <button class="page-link" onClick={goToPreviousPage}>
-              Previous
-            </button>
-          </li>
-          <li class="page-item">
-            <button class="page-link" onClick={goToNextPage}>
-              Next
-            </button>
-          </li>
-        </ul>
-      </nav>
+        <nav
+          aria-label="..."
+          className="col-lg-9 col-md-9 d-flex justify-content-center mt-3 w-100"
+        >
+          <ul className="pagination">
+            <li className="page-item">
+              <button className="page-link" onClick={goToPreviousPage}>
+                Previous
+              </button>
+            </li>
+            <li className="page-item">
+              <button className="page-link" onClick={goToNextPage}>
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
